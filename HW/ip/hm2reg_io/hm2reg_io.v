59c14543789c6194a8130d20d4417d6b159002a3
the-snowwhite <producer@holotronic.dk>
2016-03-05 12:14:11 +0100

    Some cleanups and improvements related to upcomming beta3 release

--- HW/ip/hm2reg_io/hm2reg_io.v
+++ HW/ip/hm2reg_io/hm2reg_io.v
@@ -1,18 +1,18 @@
 module hm2reg_io (
         // signals to connect to an Avalon clock source interface
         input clk,
-	input reset,
+	input reset_n,
 	// signals to connect to an Avalon-MM slave interface
 	input [ADDRESS_WIDTH-1:0] slave_address,
 	input slave_read,
 	input slave_write,
-	output [DATA_WIDTH-1:0] slave_readdata,
+	output reg [DATA_WIDTH-1:0] slave_readdata,
 	input [DATA_WIDTH-1:0] slave_writedata,
 	input slave_chipselect,
 //	input slave_waitreq,
         // signals to connect to custom con logic
 //	output waitrequest,
-	output [DATA_WIDTH-1:0] con_dataout,
+	output reg [DATA_WIDTH-1:0] con_dataout,
 	input [DATA_WIDTH-1:0] con_datain,
 	output [ADDRESS_WIDTH-1:0] con_adrout,
 	output con_write_out,
@@ -23,9 +23,8 @@ module hm2reg_io (
 parameter ADDRESS_WIDTH = 14;          // address size width
 parameter DATA_WIDTH = 32;          // word size of each input and output register
 
-
-reg [DATA_WIDTH-1:0] indata;
-reg [DATA_WIDTH-1:0] outdata;
+//reg [DATA_WIDTH-1:0] indata;
+//reg [DATA_WIDTH-1:0] outdata;
 //reg write_delay;
 //reg reg_w_act;
 
@@ -34,8 +33,8 @@ reg [DATA_WIDTH-1:0] outdata;
 
 assign con_chip_sel = slave_chipselect;
 //assign dataout = (!read && write_active && !waitreq) ? outdata : 32'bz;
-assign con_dataout = outdata;
-assign slave_readdata = indata;
+//assign con_dataout = outdata;
+//assign slave_readdata = indata;
 assign con_adrout[ADDRESS_WIDTH-1:0] = slave_address[ADDRESS_WIDTH-1:0]; 
 assign con_read_out = slave_read;
 assign con_write_out = slave_write;
@@ -48,14 +47,16 @@ end
 */
 
 always @(posedge clk) begin
-	if (reset) begin
-		indata[DATA_WIDTH-1:0] <= 32'b0;
-		outdata[DATA_WIDTH-1:0] <= 32'b0;
-        end
-	else if (slave_read)
-		indata[DATA_WIDTH-1:0] <= con_datain[DATA_WIDTH-1:0];
-	else if	(slave_write)
-		outdata <= slave_writedata;
+	if (!reset_n) begin
+		slave_readdata[DATA_WIDTH-1:0] <= 32'b0;
+		con_dataout[DATA_WIDTH-1:0] <= 32'b0;
+	end
+	else if (slave_read) begin
+		slave_readdata[DATA_WIDTH-1:0] <= con_datain[DATA_WIDTH-1:0];
+	end
+	else if	(slave_write) begin
+		con_dataout <= slave_writedata;	
+	end
 end
 
 endmodule
