13fc09854157ed23fac014eaab7cd1093c8681cf
Michael Haberler <mail17@mah.priv.at>
2016-04-17 08:55:13 +0200

    good to enable dts/dtb

--- Scripts/travis_build.sh
+++ Scripts/travis_build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export PATH="$PATH:/root/altera_lite/15.1/quartus/bin/:/root/altera_lite/15.1/quartus/sopc_builder/bin/"
 cd /work/HW/QuartusProjects/DE0_NANO_SOC_GHRD/
-make -j$(nproc) rbf # dts dtb
+make -j$(nproc) rbf dts dtb
