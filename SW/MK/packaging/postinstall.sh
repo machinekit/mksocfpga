ad42a029fffbccd087ed1970d2f666d4a5fb7298
Michael Haberler <git@mah.priv.at>
2016-07-20 13:33:17 +0200

    dtbo: reorganize - create a dts file per-rbf from template, then create a dtbo
    running make in /lib/firmware/socfgpa/dtbo does this
    postinstall.sh does exactly that

--- SW/MK/packaging/postinstall.sh
+++ SW/MK/packaging/postinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 cd /lib/firmware/socfpga/dtbo
 echo rebuilding device tree overlays in ${PWD} 
-make DTC=/usr/src/linux-headers-4.1.17-ltsi-rt17-gab2edea/scripts/dtc/dtc all
+make all
 
