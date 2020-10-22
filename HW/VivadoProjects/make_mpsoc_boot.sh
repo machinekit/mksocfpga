c39d5ad1a0e26187b032321785c4f02a7842ac4c
Holotronic <producer@holotronic.dk>
2020-10-22 20:24:57 +0200

    Add bps's for the ultra96's and a petalinux build script Generates the boot files (boot.bin, uimage.ub)
    Signed-off-by: Holotronic <producer@holotronic.dk>

---
+++ HW/VivadoProjects/make_mpsoc_boot.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Builds Boot files for the mpsoc HW using Petalinux. Pass in the board name
+# for the project you want to build. Expects Xilinx tools to be installed in
+# /opt/Xilinx like in the docker image.
+
+usage () {
+    echo "Usage: make_mpsoc_boot.sh boardname"
+    exit 1
+}
+
+if [ -z "$1" ]; then
+    usage
+fi
+
+CUR_DIR=`realpath .`
+
+
+if [[ $1 == *"ultra96"* ]]; then
+    cd /work/avnet/ultra96
+    BOARD_PART="xczu3eg"
+else
+    echo "cant't find board project folder"
+    exit 1    
+fi
+
+petalinux-create -t project -s "$1"-mk-2019.1.bsp
+cd "$1"-mk-2019.1
+time petalinux-config --get-hw-description=../"$1"_"$BOARD_PART"_created/"$1"_"$BOARD_PART".sdk --silentconfig
+time petalinux-build
+petalinux-package --boot --fsbl images/linux/zynqmp_fsbl.elf --u-boot=images/linux/u-boot.elf --pmufw --atf --fpga images/linux/system.bit --force
+tar -zxf ./images/linux/rootfs.tar.gz ./lib/modules  && tar -zcf ../lib.tar.gz ./lib && rm -r lib
